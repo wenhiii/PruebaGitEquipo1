e70bc7236a9969e79d1d6717e22db40efb59086d
DanielRFigueroa <dannyrfcr710@gmail.com>
2023-11-11 12:56:48 +0100

    V0.3 david si que si que se la come

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Joderrrr macho ostia viva españa viva el rey");
         System.out.println("ctm github");
         System.out.println("A");
+        System.out.println("david se la come");
 
         // Press Ctrl+F5 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
